9eac80379401c013c8d71098f7467d64f9646ff5
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-03-10 16:36:52 +0530

    Update main.tf

--- infra/modules/eventbridge/main.tf
+++ infra/modules/eventbridge/main.tf
@@ -15,7 +15,7 @@ resource "aws_cloudwatch_event_rule" "rule1" {
   name = "Rule1"
   event_bus_name = aws_cloudwatch_event_bus.event_bus.name
   event_pattern = jsonencode({
-    "source": ["my_lambda_function_usecase9"],
+    "source": ["lambdaFunction_SQS"],
     "detail-type": ["client-details"],
     "detail": {
       "client-type": ["gold"]
