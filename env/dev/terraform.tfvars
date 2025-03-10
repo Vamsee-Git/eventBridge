f3792ce9ef02885d2ee98c9201931290feb3de63
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-03-10 14:43:10 +0530

    infra

---
+++ env/dev/terraform.tfvars
@@ -0,0 +1,6 @@
+aws_region = "us-west-2"
+#sqs_queue_names       = ["queue1", "queue2"]
+lambda_function_name  = "my_lambda_function_usecase9"
+lambda_runtime        = "python3.13"
+event_bus_name        = "my_event_bus1"
+api_gateway_stage_name = "prod"
---
+++ infra/main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "vamsi1-terraform-bucket"
+    key            = "usecase9/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
+
+
+
+module "eventbridge" {
+  source = "./modules/eventbridge"
+  #sqs_queues = module.sqs.sqs_queues
+  event_bus_name = var.event_bus_name
+}
+
+module "lambda" {
+  source = "./modules/lambda"
+  event_bus_arn = module.eventbridge.event_bus_arn
+  lambda_runtime = var.lambda_runtime
+  lambda_function_name = var.lambda_function_name
+
+}
+
+module "api_gateway" {
+  source = "./modules/api_gateway"
+  #lambda_function_arn = module.lambda.lambda_function_arn
+  api_gateway_stage_name = var.api_gateway_stage_name
+  lambda_invoke_arn = module.lambda.invoke_arn
+  lambda_function_name = module.lambda.function_name
+}
