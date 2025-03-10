a597795a04092216cb1e50feb43ac6ea8c32a498
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-03-10 15:37:59 +0530

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket-two-tier-vamsee"
-    key            = "terraform/eventBridge_statefile"
+    key            = "terraform/eventBridge/statefile"
     region         = "ap-south-1"
     encrypt        = true
   }
